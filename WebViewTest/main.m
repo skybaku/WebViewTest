ad7b390cb0eaa4ce99ff2325495aec904361a0a7
elantia <bruce.lan@ericsson.com>
2015-04-22 19:10:58 +0800

    Initial Commit

---
+++ WebViewTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WebViewTest
+//
+//  Created by Bruce Lan on 4/22/15.
+//  Copyright (c) 2015 Bruce Lan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
